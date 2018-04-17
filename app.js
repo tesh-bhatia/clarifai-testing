06df1ecf8bbb73db84649d91938c55f64c7cedf7
tesh-bhatia <nbhatia03@gmail.com>
2018-04-17 16:01:23 -0400

    API-calling functions now take parameters, general workflow done

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const clarifai = require('clarifai');
 const dotenv = require('dotenv')
+const fs = require('fs')
 
+
+//add images to concept w/ concept -> attach to model -> train model
 dotenv.config()
 
 const clarifaiApp =  new Clarifai.App({
@@ -8,44 +11,31 @@ const clarifaiApp =  new Clarifai.App({
 });
 
 const modelID = 'test-model'
+const testPhoto = fs.readFileSync('./images/cookies.jpeg', 'base64');
 
-function createConcept () {
-    
-}
+const bezosArr = ['https://static.techspot.com/images2/news/bigimage/2018/01/2018-01-09-image-17.jpg', 'https://cdn-images-1.medium.com/max/799/1*3U0r3rpqtU0L3AHJ0ygzig.jpeg', 'https://fortunedotcom.files.wordpress.com/2016/03/bez04_2.jpg', 'https://timedotcom.files.wordpress.com/2017/04/time-100-2017-jeff-bezos.jpg?h=580', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZafkPAnJ62MZ8iQb9tamEGZFm90VZXtSk5lHa2FnH6Iu07U3y']
 
-function createImages () {
-    clarifaiApp.inputs.create([
-        {
-          url: "https://i5.walmartimages.com/asr/d4a16047-59cf-4d1e-a023-23f42a220cb1_1.c78bc362ea50b851cd08a243cc6b4ff9.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
-          id: 'train1',
-          concepts: [
-            {
-                id: "cookies",
-                value: true
-              }
-          ]
-        },
-        {
-          url: "http://cdn3.volusion.com/qsyod.mrpqw/v/vspfiles/photos/014100074359-2.jpg",
-          id: 'puppy1',
-          concepts: [
-            {
-                id: "cookies",
-                value: true
-              }
-          ]
-        },
-        {
-            url: "http://www.thegrocerygirls.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/I/C/ICBEL.jpg.jpg",
-            id: 'cookies1',
+const oreosArr = ['https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/oreolead-1494013639.png?crop=1.00xw:1.00xh;0,0&resize=768:*', 'https://smedia.webcollage.net/rwvfp/wc/cp/26597639/module/mondelez/_cp/products/1474255996629/tab-0b4e8cc1-18ba-4767-a72c-08d3a822c7d2/803597f3-b237-42d8-a2d2-1115f8d9eb69.jpg.w960.jpg', 'https://smedia.webcollage.net/rwvfp/wc/cp/26597639/module/mondelez/_cp/products/1474255996629/tab-0b4e8cc1-18ba-4767-a72c-08d3a822c7d2/284500ef-ad6d-430a-9e75-aa7e0c79da96.JPG.w960.jpg']
+
+
+
+function createImages (imagesArr, conceptName) {
+    const inputArr = imagesArr.map((image, index) => {
+        return ({
+            url: image,
+            id: conceptName + index,
             concepts: [
                 {
-                    id: "cookies",
+                    id: conceptName,
                     value: true
-                  }
+                }
             ]
-        }
-      ]).then(
+        })
+    })
+
+    console.log(inputArr)
+
+    clarifaiApp.inputs.create(inputArr).then(
         function(response) {
           // do something with response
           console.log(response)
@@ -56,22 +46,25 @@ function createImages () {
           console.log(err)
           console.log('FAILURE!')
         }
-      );
+    );
 }
 
-function addConceptToModel () {
+function addConceptToModel (modelID, conceptName) {
     console.log('running...')
     clarifaiApp.models.initModel(modelID).then(function(model) {
-        updateModel,
+        console.log('first function running')
+        console.log(model)
+        updateModel(model, conceptName)},
         function(err) {
           // there was an error
           console.log('Model not found?')
         }
-      });
+    );
 }
 
-function updateModel(model) {
-    model.mergeConcepts({"id": "cookies"}).then(
+function updateModel(model, conceptName) {
+    console.log('second function running')
+    model.mergeConcepts({"id": conceptName}).then(
       function(response) {
         // do something with response
         console.log('Concept merged!')
@@ -84,7 +77,7 @@ function updateModel(model) {
     );
   }
 
-function trainModel () {
+function trainModel (modelID) {
     clarifaiApp.models.train("test-model").then(
         function(response) {
           // do something with response
@@ -97,4 +90,32 @@ function trainModel () {
       );
 }
 
-trainModel()
+function predictContent (img) {
+    clarifaiApp.models.predict("test-model", [img]).then(
+        function(response) {
+            // do something with response
+            console.log('Predicting...')
+            console.log(response.rawData.outputs[0].data)
+        },
+        function(err) {
+            // there was an error
+            console.log('Fuck')
+        }
+    );
+}
+
+// createConcept(modelID, 'bezos')
+
+// clarifaiApp.concepts.list().then(
+//     function(response){
+//         console.log(response)
+//     },
+//     function(err){
+//         console.log(err)
+//     }
+// )
+
+// createImages(oreosArr, 'oreos')
+
+predictContent('https://specials-images.forbesimg.com/imageserve/5a8d920d31358e4955adf197/416x416.jpg?background=000000&cropX1=755&cropX2=2357&cropY1=494&cropY2=2097')
+
